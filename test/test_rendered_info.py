a3665d7765c17597d2f397b17e77654dfc88f29f
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2022-11-16 21:14:47 +0100

    add test on movies info

---
+++ test/test_rendered_info.py
@@ -0,0 +1,15 @@
+from api_modules.imdb_render import RenderedMovieInfo
+from api_modules.imdb_render import MovieInfo
+
+
+class TestRenderedMovies:
+
+    def test_rendered_info(self):
+        rendered_movies = RenderedMovieInfo.get_rendered_movie_info("Inception")
+        assert isinstance(rendered_movies[0],MovieInfo)
+
+    def test_movie_rating(self):
+        rendered_movies = RenderedMovieInfo.get_rendered_movie_info("Inception")
+        assert isinstance(rendered_movies[0].rating,int)
+
+
