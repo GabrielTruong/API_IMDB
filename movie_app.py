3a45c6b04706da07ee804e8d83c2a251f23f7168
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2022-11-17 21:31:37 +0100

    change main for api

--- movie_app.py
+++ movie_app.py
@@ -7,7 +7,8 @@ st.header("Welcome to Friday Movie !")
 st.write("You were about to watch a movie but you want to make sure it is good ? Convince yourself it is the right one by watching the trailer !")
 
 title = st.text_input('Enter the title of the movie', 'Inception')
-movie_list = RenderedMovieInfo.get_rendered_movie_info(title)
+
+movie_list = RenderedMovieInfo.get_rendered_movie_info(st.secrets["api_key"],title)
 movie_chosen = movie_list[0]
 
 st.write(f'The movie is rated {movie_chosen.rating}/10')
