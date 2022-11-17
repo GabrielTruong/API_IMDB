258a3bbfe877125ffcf22bc5fe3ecc1efcc08c3d
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2022-11-17 20:24:42 +0100

    minor fix indentation

--- test/test_rendered_info.py
+++ test/test_rendered_info.py
@@ -5,8 +5,7 @@ from unittest.mock import Mock
 
 class TestRenderedMovies(unittest.TestCase):
     def test_movie_info(self):
-        rendered_movie_test = MovieInfo(
-            "Inception", "tt1234567", "201O, Leonardo Di Caprio", "test.png")
+        rendered_movie_test = MovieInfo("Inception", "tt1234567", "201O, Leonardo Di Caprio", "test.png")
 
         assert isinstance(rendered_movie_test.title, str)
     
