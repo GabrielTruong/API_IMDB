c427d4e855a163c26ac4a8a4f4f454f8bc5bbc8a
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2022-11-18 21:55:00 +0100

    add exception for more robustness

--- movie_app.py
+++ movie_app.py
@@ -5,16 +5,24 @@ from api_modules.imdb_render import RenderedMovieInfo
 st.header("Welcome to Friday Movie !")
 st.write("You were about to watch a movie but you want to make sure it is good ? Convince yourself it is the right one by watching the trailer !")
 
-title = st.text_input('Enter the title of the movie', 'Inception')
-#st.write(f'API KEY is {st.secrets["api_key"]}')
-movie_list = RenderedMovieInfo.get_rendered_movie_info(title)
-movie_chosen = movie_list[0]
-movie_chosen.get_movie_rating()
-movie_chosen.get_movie_trailer()
+movie_name = ""
+try:
+    title = st.text_input('Enter the title of the movie', movie_name)
 
-st.write(f'The movie is rated {movie_chosen.rating}/10')
-st.write('The trailer will appear below: ')
-st.video(movie_chosen.trailer)
+#st.write(f'API KEY is {st.secrets["api_key"]}')
+    movie_list = RenderedMovieInfo.get_rendered_movie_info(title)
+    movie_chosen = movie_list[0]
+    movie_chosen.get_movie_rating()
+    movie_chosen.get_movie_trailer()
+    st.write(f'The movie is rated {movie_chosen.rating}/10')
+    st.write('The trailer will appear below: ')
+    st.video(movie_chosen.trailer)
+except IndexError:
+    st.write("This movie doesn't exist")
+except TypeError:
+    print("Empty string")
+
+
 
 
 
